1878fa289f69e54254182f7e96ea655b30980277
s25abelmiku <michisillproductions@gmail.com>
2026-02-27 17:46:36 +0200

    Edit: added get by id controller + error

--- src/main/java/lv/venta/controller/SimpleController.java
+++ src/main/java/lv/venta/controller/SimpleController.java
@@ -1,6 +1,5 @@
 package lv.venta.controller;
 
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
@@ -8,18 +7,18 @@ import java.util.Random;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lv.venta.model.Category;
 import lv.venta.model.Product;
 
 @Controller
 public class SimpleController {
-	
-	ArrayList<Product> allProducts = new ArrayList<>(Arrays.asList(
-			new Product("Wafele", 100f, "Mēma", Category.other,19000),
-			new Product("Suns", 920.22f, "Labs", Category.other,1),
-			new Product("Āmurs", 22f, "Sit ok", Category.other,4)
-			));
+
+	ArrayList<Product> allProducts = new ArrayList<>(
+			Arrays.asList(new Product("Wafele", 100f, "Mēma", Category.other, 19000),
+					new Product("Suns", 920.22f, "Labs", Category.other, 1),
+					new Product("Āmurs", 22f, "Sit ok", Category.other, 4)));
 
 	@GetMapping("/showmessage")
 	public String getShowMessage() {
@@ -35,6 +34,7 @@ public class SimpleController {
 		model.addAttribute("package", text);
 		return "data";
 	}
+
 	@GetMapping("/getproduct")
 	public String getGetProduct(Model model) {
 		System.out.println("Reku Manta");
@@ -42,9 +42,26 @@ public class SimpleController {
 		model.addAttribute("package", product);
 		return "prod";
 	}
+
 	@GetMapping("/getallproducts")
 	public String getGetAllProducts(Model model) {
-		model.addAttribute("package",allProducts);
+		model.addAttribute("package", allProducts);
 		return "showall";
+	}
+
+	@GetMapping("/getallproducts/{id}")
+	public String getGetAllProductsById(@PathVariable(name = "id") int id, Model model) {
+		if (id < 0) {
+			model.addAttribute("package", "nebuus");
+			return "error-page";
+		}
+		for (Product tempP : allProducts) {
+			if (tempP.getId() == id) {
+				model.addAttribute("pacakge", tempP);
+				return "show-one-product";
+			}
+		}
+		model.addAttribute("package","Produkts nava");
+		return "error-page";
 	}
 }
