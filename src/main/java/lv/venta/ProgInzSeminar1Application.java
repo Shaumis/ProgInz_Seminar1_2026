bd27b1dc745ec1b9832df220c42e29324e6ea832
s25abelmiku <michisillproductions@gmail.com>
2026-03-13 18:21:36 +0200

    DB stuff

--- src/main/java/lv/venta/ProgInzSeminar1Application.java
+++ src/main/java/lv/venta/ProgInzSeminar1Application.java
@@ -14,22 +14,32 @@ public class ProgInzSeminar1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgInzSeminar1Application.class, args);
-		
+
 	}
+
 	@Bean
 	public CommandLineRunner saveDataInDB(IProductRepo prodRepo) {
 		return new CommandLineRunner() {
-			
+
 			@Override
 			public void run(String... args) throws Exception {
 				Product p1 = new Product("Ābols", 0.99f, "Salds un garšīgs", Category.computer, 10);
 				Product p2 = new Product("Burkāns", 2.84f, "Oranžš", Category.medicine, 40);
 				Product p3 = new Product("Vīnogas", 4.99f, "Violetas", Category.other, 3);
-				
+
 				prodRepo.save(p1);
 				prodRepo.save(p2);
 				prodRepo.save(p3);
 
+				System.out.println("Ierakstu skaits :" + prodRepo.count());
+				System.out.println("Prod ar id=1 " + prodRepo.findById(1).get());
+				System.out.println("All: " + prodRepo.findAll());
+				prodRepo.deleteById(1);
+				System.out.println("All: " + prodRepo.findAll());
+				Product productForUpdate = prodRepo.findById(2).get();
+				productForUpdate.setPrice(9.99f);
+				prodRepo.save(productForUpdate);
+				System.out.println("All: " + prodRepo.findAll());
 			}
 		};
 	}
