c680856335c1335e505377acd3504ba8553aa1ba
s25abelmiku <michisillproductions@gmail.com>
2026-03-13 18:39:04 +0200

    CRUD

---
+++ src/main/java/lv/venta/service/ICRUDProductService.java
@@ -0,0 +1,20 @@
+package lv.venta.service;
+
+import java.util.ArrayList;
+
+import lv.venta.model.Category;
+import lv.venta.model.Product;
+
+public interface ICRUDProductService {
+
+	public abstract Product createNewProduct(Product newProduct)throws Exception;
+	
+	public abstract ArrayList<Product> retrieveAllProducts() throws Exception;
+	
+	public abstract Product retrieveProductbyId(int id) throws Exception;
+	
+	public abstract Product updateProductById(int id, float price, Category category, String description,int quantity)throws Exception;
+	
+	public abstract void deleteProduct(int id) throws Exception;
+	
+}
